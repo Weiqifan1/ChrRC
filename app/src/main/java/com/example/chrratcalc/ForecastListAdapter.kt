96d7097d883493cf5abdfd0f8479e5555b3978e6
Weiqifan1 <cmlykke@hotmail.com>
2019-03-08 11:19:39 +0100

    jeg har fulgt kotlinForAndroidDevelopers frem til side 36/226. Koden virker ikke, da MainActivity.kt ikke acceptere RecyclerView. Det er nok foraarsaget af at app/build.gradle ikke kan laese recyclerview

---
+++ app/src/main/java/com/example/chrratcalc/ForecastListAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.chrratcalc
+
+class ForecastListAdapter(val items: List<String>):
+    RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    
+}
+
+
+
+
+
+
+//end
--- app/src/main/java/com/example/chrratcalc/MainActivity.kt
+++ app/src/main/java/com/example/chrratcalc/MainActivity.kt
@@ -4,11 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+
 class MainActivity : AppCompatActivity() {
+
+
+    val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+    forecastList.layoutManager = LinearLayoutManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        messageChr.text = "Hello kotlin! chr (fra MainAcrivity.kt)"//KotlinForAndroidDevs 28/226
+        //messageChr.text = "Hello kotlin! chr (fra MainAcrivity.kt)"//KotlinForAndroidDevs 28/226
+
     }
 }
